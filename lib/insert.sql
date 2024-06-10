17068eac03ae452db57effe93789dae039726d62
Brendan Dempsey <bjd1412@gmail.com>
2024-06-10 03:27:57 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Mr. Chocolate", 12, "M", "Brown", "Chillen", True);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Rowdy", 6, "M", "Red", "Rash", True);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Tabitha", 5, "F", "Purple", "Serious", True);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Seargent Brown", 20, "M", "Brown", "Hardy", True);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Melisa", 9, "F", "Violet", "Sassy", True);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Grinch", 8, "M", "Green", "Lonely", True);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES("Wendy", 13, "F", "Black", "Jolly", True);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(NULL, 2, "M", "Red", "Impish", False);
